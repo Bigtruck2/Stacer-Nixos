2b9bd779fec4b3545a7070203571545b352b6ffc
Bigtruck2 <bigburrito363@gmail.com>
2026-09-05 04:34:57 -0400

    add aarch64 to nix flake

--- flake.nix
+++ flake.nix
@@ -5,49 +5,65 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-  packages.${system}.default = pkgs.stdenv.mkDerivation {
-        pname = "stacer";
-        version = "1.7.0";
-
-        src = ./.;
-        #hardeningDisable = [ "fortify" ];
-
-        nativeBuildInputs = with pkgs; [
-          cmake
-          qt6.qttools
-          pkgs.qt6.wrapQtAppsHook
-        ];
-        buildInputs = [
-          pkgs.qt6.qtbase
-          pkgs.qt6.qtcharts
-        ];
-        cmakeBuildType = "Debug";
-
-        meta = {
-          description = "Linux System Optimizer and Monitoring";
-          mainProgram = "stacer";
-          platforms = pkgs.lib.platforms.linux;
-        };
-      };
-
-      apps.${system}.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/stacer";
-      };
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        cmake
-        qt6.qttools
-        qt6.qtbase
-        qt6.qtcharts
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
       ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "stacer";
+            version = "1.7.0";
+
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              cmake
+              qt6.qttools
+              qt6.wrapQtAppsHook
+            ];
+
+            buildInputs = with pkgs; [
+              qt6.qtbase
+              qt6.qtcharts
+            ];
+
+            cmakeBuildType = "Debug";
+
+            meta = {
+              description = "Linux System Optimizer and Monitoring";
+              mainProgram = "stacer";
+              platforms = pkgs.lib.platforms.linux;
+            };
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/stacer";
+        };
+      });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cmake
+              qt6.qttools
+              qt6.qtbase
+              qt6.qtcharts
+            ];
+          };
+        });
     };
-  };
 }
